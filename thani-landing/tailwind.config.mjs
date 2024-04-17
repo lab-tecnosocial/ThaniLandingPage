4b7cee9ec6c1492045db0bcf7d400bb83a0726b8
Joel Alvarez <eynar.j.alvarez.m@gmail.com>
2024-04-16 20:44:07 -0400

    feat: migrate landing page

--- thani-landing/tailwind.config.mjs
+++ thani-landing/tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			textos: '#505050',
+			thani100: '#086E7D',
+			thani200: '#FFC900',
+			background: '#FAFAFA',
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('tailwindcss-animated')
+	],
 }
